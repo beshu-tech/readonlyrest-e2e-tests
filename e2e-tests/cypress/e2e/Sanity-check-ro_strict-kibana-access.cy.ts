c12d0a77c66190ec9c9b14f6b7508a6daf7f3f41
Dawid Poliszak <dawid@readonlyrest.com>
2024-09-14 08:04:26 +0200

    test

--- e2e-tests/cypress/e2e/Sanity-check-ro_strict-kibana-access.cy.ts
+++ e2e-tests/cypress/e2e/Sanity-check-ro_strict-kibana-access.cy.ts
@@ -8,6 +8,13 @@ describe('sanity check ro_strict kibana access', () => {
   beforeEach(() => {
     Login.initialization();
   });
+
+  afterEach(() => {
+    Settings.setSettingsData(defaultSettings);
+    cy.deleteRequest({
+      url: `${Cypress.config().baseUrl}/api/sample_data/ecommerce`,
+      header: 'x-ror-current-group: template_group'
+    });
   it('should verify that everything works', () => {
     RoAndRoStrictKibanaAccessAssertions.runAssertions(roStrictSettings);
   });
