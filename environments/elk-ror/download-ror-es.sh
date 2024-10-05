d4dc8b0667ddc74806da4731c764b3eadc3fe601
Mateusz Kołodziejczyk <coutopl@gmail.com>
2024-10-05 20:32:15 +0200

    clean up

--- environments/elk-ror/download-ror-es.sh
+++ environments/elk-ror/download-ror-es.sh
@@ -9,9 +9,9 @@ fi
 
 ES_VERSION="$1"
 
-mkdir -p ../plugins
+mkdir -p images/plugins
 ES_PLUGIN_FILENAME="ROR-latest-for-ES-$ES_VERSION.zip"
 
-curl -s -L -D - -o "../plugins/$ES_PLUGIN_FILENAME" "https://api.beshu.tech/download/es?esVersion=$ES_VERSION&email=ror-e2e-tests%40readonlyrest.com" > /dev/null 2>&1
+curl -s -L -D - -o "images/plugins/$ES_PLUGIN_FILENAME" "https://api.beshu.tech/download/es?esVersion=$ES_VERSION&email=ror-e2e-tests%40readonlyrest.com" > /dev/null 2>&1
 
 echo "$ES_PLUGIN_FILENAME"
