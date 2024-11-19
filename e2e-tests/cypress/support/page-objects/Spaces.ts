a51684b65ca7af89addb40405807d9cc2e31a7b5
Dawid Poliszak <dawid@readonlyrest.com>
2024-11-19 06:29:24 +0100

    test fixes

--- e2e-tests/cypress/support/page-objects/Spaces.ts
+++ e2e-tests/cypress/support/page-objects/Spaces.ts
@@ -4,16 +4,21 @@ import { getKibanaVersion } from '../helpers';
 export class Spaces {
   static removeSpace(spaceName: string) {
     cy.log('Remove space');
+    const spaceNameLowerCaseAndDash = spaceName.toLowerCase().replace(' ', '-');
 
     cy.get('[data-test-subj=spacesNavSelector]').click();
     cy.get('[data-test-subj=manageSpaces]').click({ force: true });
     if (semver.gte(getKibanaVersion(), '8.16.0')) {
-      spaceName = spaceName.toLowerCase();
-      cy.get(`[id="${spaceName}-space-actions"]`).click();
+      cy.get(`[id="${spaceNameLowerCaseAndDash}-actions"]`).click();
+      cy.get(`[data-test-subj="${spaceNameLowerCaseAndDash}-deleteSpace"]`).click();
+    } else {
+      cy.get(`[data-test-subj="${spaceName}-deleteSpace"]`).click();
     }
 
-    cy.get(`[data-test-subj="${spaceName}-space-deleteSpace"]`).click();
+    cy.intercept(`s/${spaceNameLowerCaseAndDash}/api/spaces/space/${spaceNameLowerCaseAndDash}`).as('deleteSpace');
     cy.get('[data-test-subj=confirmModalConfirmButton]').click({ force: true });
-    cy.contains(`Deleted space '${spaceName}'`);
+    cy.wait('@deleteSpace').then(({ response }) => {
+      expect([204]).to.include(response.statusCode);
+    });
   }
 }
