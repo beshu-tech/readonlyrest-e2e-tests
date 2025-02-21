b92de565bfefc9f2281deb4d101b3a7b1c1d604b
Dawid Poliszak <dawid@readonlyrest.com>
2025-02-21 07:01:55 +0100

    RORDEV-1374 Support reporting data stream multitenancy for->=-Kibana-8.15.0 (#29)
    Co-authored-by: Mateusz Kołodziejczyk <coutopl@gmail.com>

--- e2e-tests/cypress/e2e/Reporting-index.cy.ts
+++ e2e-tests/cypress/e2e/Reporting-index.cy.ts
@@ -3,9 +3,14 @@ import { Loader } from '../support/page-objects/Loader';
 import { RorMenu } from '../support/page-objects/RorMenu';
 import { Discover } from '../support/page-objects/Discover';
 import { Settings } from '../support/page-objects/Settings';
+import { KibanaNavigation } from '../support/page-objects/KibanaNavigation';
+import * as semver from 'semver';
+import { getKibanaVersion } from '../support/helpers';
+import { kbnApiAdvancedClient } from '../support/helpers/KbnApiAdvancedClient';
 
-// todo: the test fails. Please fix me
-describe.skip('Reporting index', () => {
+describe('Index Pattern Configuration for Reporting index', () => {
+  const admin = 'admin:dev';
+
   beforeEach(() => {
     Settings.setSettingsData('reportingSettings.yaml');
     cy.visit(Cypress.config().baseUrl);
@@ -18,15 +23,26 @@ describe.skip('Reporting index', () => {
   });
 
   afterEach(() => {
+    kbnApiAdvancedClient.deleteSavedObjects(admin, 'infosec_group');
+    if (semver.gte(getKibanaVersion(), '8.0.0')) {
+      kbnApiAdvancedClient.deleteDataViews(admin, 'infosec_group');
+    }
     Settings.setSettingsData('defaultSettings.yaml');
   });
 
   it('should correctly match index pattern when audit index_template contains .reporting', () => {
     const indexPattern = 'xxx.reporting';
     RorMenu.changeTenancy('Infosec', '/app/home#/');
+    KibanaNavigation.openKibanaNavigation();
+    KibanaNavigation.openPage('Stack Management');
+    if (semver.gte(getKibanaVersion(), '8.0.0')) {
+      KibanaNavigation.openSubPage('Data Views');
+    } else {
+      KibanaNavigation.openSubPage('Index Patterns');
+    }
     Discover.createIndexPattern(indexPattern);
-    cy.contains('timestamp:').should('be.visible');
-    cy.contains('acl_history:').should('be.visible');
+    cy.contains('@timestamp').should('be.visible');
+    cy.contains('acl_history').should('be.visible');
     cy.contains(indexPattern).should('be.visible');
   });
 });
