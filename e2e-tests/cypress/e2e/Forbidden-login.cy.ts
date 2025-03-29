b2561aec5f80ef24c8a4db2a03954dc227e30541
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2025-03-29 07:51:32 +0100

    [RORDEV-1204] e2e test for custom forbidden message (#38)

---
+++ e2e-tests/cypress/e2e/Forbidden-login.cy.ts
@@ -0,0 +1,31 @@
+import { Login } from '../support/page-objects/Login';
+import { Loader } from '../support/page-objects/Loader';
+import { Settings } from '../support/page-objects/Settings';
+
+describe('Forbidden login test', () => {
+  before(() => {
+    Settings.setSettingsData('defaultSettings.yaml');
+  });
+
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.visit('/');
+  });
+
+  it('should be able to login after a failed attempt with incorrect credentials', () => {
+    Login.fillLoginPageWithWrongCredentials();
+    
+    cy.get('#form-message')
+      .should('be.visible')
+      .and('contain.text', 'You shall not pass!');
+    
+    cy.get('#form-username').clear();
+    cy.get('#form-password').clear();
+    Login.fillLoginPage();
+    
+    Loader.loading();
+    
+    cy.url().should('include', '/s/default/app/home');
+  });
+}); 
