f8769c48ce6b92a937279e4874b1fa6b3b5d3691
Dawid Poliszak <dawid@readonlyrest.com>
2025-03-30 13:04:20 +0200

    RORDEV-1406 fix apm build for 9.0.0-beta1

--- environments/elk-ror/images/apm/version_check.sh
+++ environments/elk-ror/images/apm/version_check.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-set -x
+
+# Require exactly 2 arguments
+if [[ $# -ne 2 ]]; then
+  echo "Usage: $0 <version1> <version2>"
+  echo "Checks if version1 is equal to or higher than version2"
+  exit 2
+fi
+
+# Ensure parameters aren't empty
+if [[ -z "$1" || -z "$2" ]]; then
+  echo "Error: Version parameters cannot be empty"
+  exit 2
+fi
+
 is_version_equal_or_higher() {
   [[ "$1" == $(echo -e "$1\n$2" | sort -V | tail -n1) ]]
 }
