e8faa4892c69bcfd45de29f806bcc91dfe536ad4
Dawid Poliszak <dawid@readonlyrest.com>
2025-11-13 07:31:35 +0100

    another try

--- e2e-tests/cypress/e2e/Sanity-check.cy.ts
+++ e2e-tests/cypress/e2e/Sanity-check.cy.ts
@@ -10,7 +10,7 @@ import { esApiAdvancedClient } from '../support/helpers/EsApiAdvancedClient';
 import { kbnApiAdvancedClient } from '../support/helpers/KbnApiAdvancedClient';
 import { SampleData } from '../support/helpers/SampleData';
 
-describe.skip('sanity check', () => {
+describe('sanity check', () => {
   beforeEach(() => {
     SampleData.createSampleData('sample_index', 1);
     Login.initialization();
