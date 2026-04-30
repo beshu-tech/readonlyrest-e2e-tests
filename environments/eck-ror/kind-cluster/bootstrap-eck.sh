ae3ba83e19ceb081e74d868269bf9fead0ac265e
Mateusz Kołodziejczyk <coutopl@gmail.com>
2026-04-30 12:56:50 +0200

    ECK monitoring

--- environments/eck-ror/kind-cluster/bootstrap-eck.sh
+++ environments/eck-ror/kind-cluster/bootstrap-eck.sh
@@ -9,3 +9,10 @@ fi
 
 kubectl create -f "https://download.elastic.co/downloads/eck/$ECK_VERSION/crds.yaml"
 kubectl apply -f "https://download.elastic.co/downloads/eck/$ECK_VERSION/operator.yaml"
+
+# Install metrics-server for kubectl top support.
+# Kind uses self-signed kubelet certs, so --kubelet-insecure-tls is required.
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl patch deployment metrics-server -n kube-system \
+  --type=json \
+  -p='[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"}]'
