0276b5829cc7e6901ff94b2f4ead10f25fff2770
Dawid Poliszak <dawid@readonlyrest.com>
2026-05-13 15:20:48 +0200

    RORDEV-2030 another fix

--- e2e-tests/cypress/support/page-objects/ManageSpaces.ts
+++ e2e-tests/cypress/support/page-objects/ManageSpaces.ts
@@ -4,7 +4,7 @@ import { getKibanaVersion } from '../helpers';
 export class ManageSpaces {
   static openSpaceViaNavSelector(spaceName: string) {
     cy.get('[data-test-subj=spacesNavSelector]').click();
-    ManageSpaces.getManageButtonInNavSelector().should('be.visible').click();
+    ManageSpaces.getManageButtonInNavSelector().click({ force: true });
     cy.contains(spaceName).click();
   }
 
