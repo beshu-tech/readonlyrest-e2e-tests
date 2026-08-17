cdc75ed43fe3165dbd71263358b02ec1c2154fd9
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2026-08-17 09:11:54 +0200

    [RORDEV-1229] Pin yarn to 1.22.22 (#105)

--- docker-based-ror-dev-env/src/processRorEnvCommand.sh
+++ docker-based-ror-dev-env/src/processRorEnvCommand.sh
@@ -5,7 +5,14 @@ cd "$(dirname "$0")"
 cd /app
 
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-npm i -g yarn
+# The tests run `yarn --frozen-lockfile`. Yarn 2 gave that flag a new name. Thus we must use yarn 1.
+# Corepack reads "packageManager" in e2e-tests/package.json. It installs that version of yarn and
+# makes sure that the hash agrees.
+# The Dockerfile pins NODE_VERSION to v24.11.0, which contains corepack. Node 25 and subsequent
+# versions do not contain it. If you increase NODE_VERSION to 25 or more, this command fails. Then
+# install corepack in the Dockerfile with `npm i -g corepack`.
+export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+corepack enable
 
 case "$1" in
     e2e-tests-7x )
