70b2a8f5b3628c2f43167c47a80251296b47e732
Mateusz Kołodziejczyk <coutopl@gmail.com>
2026-08-21 17:49:44 +0200

    wip

--- e2e-tests/cypress/support/page-objects/Spaces.ts
+++ e2e-tests/cypress/support/page-objects/Spaces.ts
@@ -1,6 +1,5 @@
 import * as semver from 'semver';
 import { getKibanaVersion } from '../helpers';
-import { ManageSpaces } from './ManageSpaces';
 
 export class Spaces {
   static removeSpace(spaceName: string) {
@@ -43,8 +42,7 @@ export class Spaces {
 
   static createNewSpace(spaceName: string) {
     cy.log('Create new space');
-    cy.get('[data-test-subj=spacesNavSelector]').click();
-    ManageSpaces.getManageButtonInNavSelector().click({ force: true });
+    cy.location('search').then(search => cy.visit(`/s/default/app/management/kibana/spaces${search}`));
     cy.get('[data-test-subj=createSpace]').click();
     cy.get('[data-test-subj=addSpaceName]').type(spaceName);
     cy.get('#featureCategoryCheckbox_kibana').uncheck();
