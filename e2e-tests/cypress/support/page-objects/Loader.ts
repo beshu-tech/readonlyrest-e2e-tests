143917c7e3e02ec914d7d8351c7ec596adbbd9b0
Mateusz Kołodziejczyk <coutopl@gmail.com>
2026-08-22 18:16:49 +0200

    wip

--- e2e-tests/cypress/support/page-objects/Loader.ts
+++ e2e-tests/cypress/support/page-objects/Loader.ts
@@ -3,17 +3,11 @@ import { TENANCY_QUERY_STRING_KEY } from '../types';
 export class Loader {
   public static loading(finishUrl?: string, spacePrefix?: string) {
     cy.log('loading');
-    this.start();
     this.finish(finishUrl, spacePrefix);
   }
 
   public static waitForBreadcrumb(breadcrumb: string) {
     cy.getByDataTestSubj('breadcrumb first last').contains(breadcrumb);
-  }
-
-  private static start() {
-    cy.log('loading start');
-    cy.contains('Loading Elastic', { timeout: 80000 }).should('exist');
   }
 
   private static finish(finishUrl = `/app/home?${TENANCY_QUERY_STRING_KEY}=*`, spacePrefix = '/s/default') {
@@ -34,6 +28,5 @@ export class Loader {
       cy.window().then(win => win.location.replace(target));
     });
     cy.urlShouldMatch(`${spacePrefix}${finishUrl}`);
-    cy.get('[data-test-subj=globalLoadingIndicator-hidden]').should('exist');
   }
 }
