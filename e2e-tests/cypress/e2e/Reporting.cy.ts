5ffb43dcf8d633a590920b0abc2b72bbebffe7dc
Dawid Poliszak <dawid@readonlyrest.com>
2026-09-01 11:51:29 +0200

    Refactor reporting e2e tests to use custom sample data

--- e2e-tests/cypress/e2e/Reporting.cy.ts
+++ e2e-tests/cypress/e2e/Reporting.cy.ts
@@ -1,20 +1,21 @@
 import * as semver from 'semver';
 import { Login } from '../support/page-objects/Login';
 import { esApiClient } from '../support/helpers/EsApiClient';
-import { Home } from '../support/page-objects/Home';
-import { kbnApiClient } from '../support/helpers/KbnApiClient';
-import { getKibanaVersion, userCredentials } from '../support/helpers';
+import { getKibanaVersion } from '../support/helpers';
 import { Discover } from '../support/page-objects/Discover';
 import { Reporting } from '../support/page-objects/Reporting';
 import { esApiAdvancedClient } from '../support/helpers/EsApiAdvancedClient';
 import { kbnApiAdvancedClient } from '../support/helpers/KbnApiAdvancedClient';
 import { IndexLifecyclesPolicies } from '../support/page-objects/IndexLifecyclesPolicies';
+import { SampleData } from '../support/helpers/SampleData';
 
 const kibanaUser: [string, string] = Cypress.env().kibanaUserCredentials.split(':');
 const nonTenantUser = { username: kibanaUser[0], password: kibanaUser[1], index: '.kibana' };
 const tenantUser = { username: Cypress.env().login, password: Cypress.env().password, index: '.kibana_admins_group' };
 
 const testData: { username: string; password: string; index: string }[] = [nonTenantUser, tenantUser];
+
+const reportingSampleIndex = 'reporting_sample_index';
 
 if (semver.gte(getKibanaVersion(), '8.15.0')) {
   testData.forEach(({ username, password, index }) => {
@@ -36,13 +37,14 @@ if (semver.gte(getKibanaVersion(), '8.15.0')) {
         kbnApiAdvancedClient.deleteSavedObjects(`${username}:${password}`);
         esApiAdvancedClient.pruneAllReportingIndices();
         esApiClient.deleteIndex(oldFormatReportingIndex);
-        kbnApiClient.deleteSampleData('ecommerce', userCredentials);
+        esApiClient.deleteIndex(reportingSampleIndex);
       });
 
       it(`should correctly display all reports from both the old reporting index and the new reporting data stream`, () => {
         Login.initialization({ credentials: { username, password } });
-        Home.loadSampleData();
+        SampleData.createSampleData(reportingSampleIndex, 1);
         Discover.openDataViewPage();
+        Discover.createIndexPattern('reporting_sample');
         Discover.saveReport(newFormatReportingName);
         Discover.exportToCsv();
         Reporting.openReportingPage('kibanaNavigation');
@@ -57,8 +59,9 @@ if (semver.gte(getKibanaVersion(), '8.15.0')) {
 
       it('should display all reports from all reporting data stream segments', () => {
         Login.initialization({ credentials: { username, password } });
-        Home.loadSampleData();
+        SampleData.createSampleData(reportingSampleIndex, 1);
         Discover.openDataViewPage();
+        Discover.createIndexPattern('reporting_sample');
         Discover.saveReport(newFormatReportingName);
         Discover.exportToCsv();
         // Let the first report land before rolling over (see waitForReportingSegmentsDocsCount).
@@ -80,13 +83,14 @@ if (semver.gte(getKibanaVersion(), '8.15.0')) {
     afterEach(() => {
       kbnApiAdvancedClient.deleteSavedObjects(`${username}:${password}`);
       esApiAdvancedClient.pruneAllReportingIndices();
-      kbnApiClient.deleteSampleData('ecommerce', userCredentials);
+      esApiClient.deleteIndex(reportingSampleIndex);
     });
     describe(`Reporting tests for ${username}`, () => {
       it('should correctly display all reporting data', () => {
         Login.initialization({ credentials: { username, password } });
-        Home.loadSampleData();
+        SampleData.createSampleData(reportingSampleIndex, 1);
         Discover.openDataViewPage();
+        Discover.createIndexPattern('reporting_sample');
         Discover.saveReport(reportingName);
         Discover.exportToCsv();
         Reporting.openReportingPage('kibanaNavigation');
